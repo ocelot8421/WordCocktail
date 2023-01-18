11c13a1d2ac0e08af6773b5346a665763cc195f5
ocelot8421 <8ocelot@gmail.com>
2023-01-18 20:17:52 +0100

    fixed renaming issue caused by compiled file

--- src/main/java/NameGiver.java
+++ src/main/java/NameGiver.java
@@ -1,45 +1,64 @@
 //Changing name of new screenshots into english word.
 //Making links for them and changing name into hungarian translation.
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static questioner.Questioner.askDirectoryLocation;
 
-public class NameGiver { //TODO
+public class NameGiver {
 
-    //Rename new screenshots from video
+    //Rename new screenshots from the video
     public void renameScreenshots() {
+
+        //Ask for the source directory
         String p = askDirectoryLocation("Where are the new screenshots? \n(For example: C:/words/english/...)");
         File pathNewImages = new File(p);
         List<String[]> contentCSV = new ArrayList<>();
         File[] dirContent = pathNewImages.listFiles();
+
+        //Define the csv with english-hungarian words
         for (File listFile : Objects.requireNonNull(dirContent)) {
             if (receiveExtension(listFile).equals("csv")) {
                 contentCSV = receiveCSVData(listFile);
             }
         }
 
+        //Make new directory named the actual data and time
         int i = contentCSV.size() - 1;
+        Path dirDated = Paths.get(pathNewImages.getParent() +
+                File.separator +
+                DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss").format(LocalDateTime.now()));
+        try {
+            Files.createDirectory(dirDated);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Make a copy of png files renamed by the english word from csv
         for (File listFile : Objects.requireNonNull(dirContent)) {
             if (receiveExtension(listFile).equals("png")) {
-                File imgRenamed = new File(listFile.getParentFile().getParentFile()
-                        + File.separator
-                        + new File(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss").format(LocalDateTime.now()))
-                        + File.separator
-                        + contentCSV.get(i)[2]
-                        + ".png");
+                Path oldName = Paths.get(listFile.getAbsolutePath());
+                Path imgRenamed = Paths.get(
+                        dirDated +
+                                File.separator +
+                                contentCSV.get(i)[2] +
+                                ".png");
+                System.out.println(imgRenamed);
                 try {
-                    FileUtils.copyFile(listFile, imgRenamed);
+                    Files.copy(oldName, imgRenamed, REPLACE_EXISTING);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -48,6 +67,7 @@ public class NameGiver { //TODO
         }
     }
 
+    //Receive all data from csv (columns: "angol", "magyar", english words, hungarian words)
     private List<String[]> receiveCSVData(File listFile) {
         List<String[]> contentCSV = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(listFile))) {
@@ -62,6 +82,7 @@ public class NameGiver { //TODO
         return contentCSV;
     }
 
+    //Receive the part of the file name after dot(".")
     private String receiveExtension(File listFile) {
         String name = listFile.getName();
         int indexExtension = name.lastIndexOf(".");
