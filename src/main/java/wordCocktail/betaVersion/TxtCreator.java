55416938743b1ce4a148ec7713c7e9ec5df4a2e4
ocelot8421 <8ocelot@gmail.com>
2023-02-07 21:21:35 +0100

    first line of txt contains the word and its translation too

--- src/main/java/wordCocktail/betaVersion/TxtCreator.java
+++ src/main/java/wordCocktail/betaVersion/TxtCreator.java
@@ -27,34 +27,38 @@ public class TxtCreator {
         String sourceDesignation = directory.getName(2) + "," +
                 directory.getName(3) + "," +
                 directory.getName(4);
-        createTxtFile(csvRow, directory, sourceDesignation, colHun);
-        createTxtFile(csvRow, directory, sourceDesignation, colEng);
+        createTxtFile(csvRow, directory, sourceDesignation, colHun, colEng);
+        createTxtFile(csvRow, directory, sourceDesignation, colEng, colHun);
     }
 
     /**
      * Makes a txt file in the directory and names it from CSV
      *
-     * @param csvRow CSV contains english and hungarian words. Imported from Google Translate. //TODO the google translate part should be README content
-     * @param dir    directory contains png files, links.
-     * @param source source of the word, taken from the directory path. //TODO from file content named "source"
-     * @param numCol index of the column where the word used for renaming is found.
+     * @param csvRow        CSV contains english and hungarian words. Imported from Google Translate. //TODO the google translate part should be README content
+     * @param dir           directory contains png files, links.
+     * @param source        source of the word, taken from the directory path. //TODO from file content named "source"
+     * @param numCol        index of the column where the word used for renaming is found.
+     * @param numColForeign translation of the word
      * @throws IOException
      **/
-    private static void createTxtFile(String[] csvRow, Path dir, String source, int numCol) throws IOException {
+    private static void createTxtFile(String[] csvRow, Path dir, String source, int numCol, int numColForeign) throws IOException {
         String word = DecodeText.decodeText(csvRow[numCol], "UTF-8");
+        String wordForeign = DecodeText.decodeText(csvRow[numColForeign], "UTF-8");
         File file = new File(dir + File.separator + word + ".txt");
-        writeStarterDate(file, source, word);
+        writeStarterDate(file, source, word, wordForeign);
     }
 
     /**
-     * Writes starter information in the first three lines of txt file: word, source, date of saving.
-     * @param  file the txt file.
-     * @param source source of the word, taken from the parent directory path.
-     * @param word english or hungarian word.
+     * Writes starter information in the first three lines of txt file: word-pair, source, date of saving.
+     *
+     * @param file        the txt file.
+     * @param source      source of the word, taken from the parent directory path.
+     * @param word        english or hungarian word.
+     * @param wordForeign translation of the word
      */
-    private static void writeStarterDate(File file, String source, String word) {
+    private static void writeStarterDate(File file, String source, String word, String wordForeign) {
         try (FileWriter writer = new FileWriter(file, StandardCharsets.UTF_8)) {
-            writer.write("word, " + word + "\n" +
+            writer.write("word, " + word + " - " + wordForeign + "\n" +
                     "source," + source + "\n" +
                     "saved," + DateTimeFormatter.ofPattern("yyyy.MM.dd.,HH:mm:ss").format(LocalDateTime.now()));
         } catch (IOException e) {
